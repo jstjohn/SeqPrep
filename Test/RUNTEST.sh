b3a11dad54a5f65150507e83d52dd93b51f7c4fe
John St. John <johnthesaintjohn@gmail.com>
2011-06-15 14:44:41 -0700

    fixed space problem in test script

--- Test/RUNTEST.sh
+++ Test/RUNTEST.sh
@@ -2,7 +2,7 @@
 #To trim these sequences and merge them:
 #pushd .. && make clean && make && popd
 ../SeqPrep -f multiplex_bad_contam_1.fq.gz \
--r multiplex_bad_contam_2.fq.gz \ 
+-r multiplex_bad_contam_2.fq.gz \
 -1 pe_bad_contam_trimmed_1.fastq.gz \
 -2 pe_bad_contam_trimmed_2.fastq.gz \
 -s pe_bad_contam_merged.fastq.gz \
